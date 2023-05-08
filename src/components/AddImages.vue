d0a31e03643607b160bd32fe93162ab39e390064
eugene233 <agboreugene@gmail.com>
2023-05-08 10:22:25 +0100

    Ui enhancements

--- src/components/AddImages.vue
+++ src/components/AddImages.vue
@@ -1,14 +1,9 @@
 <template>
     <NavBar />
     <p v-bind="this.message" hidden="{this.message}" class="alert alert-info" >{this.message}</p>
-    <div class="container text-center  ">
+    <div class="container text-center">
         <span>
-            <div class="category-text">
-                <div style="float:left">
-                    <h5>Enter a Wikimedia Commons category </h5>
-                    <input class="" v-model="this.category" type="text" placeholder="Category:African Foods" />
-                </div>
-                <div class="type-select">
+            <div class="type-select">
                     <h5>Select a type</h5>
                     <select v-model="this.selected_category_type" class="cat-type">
                     <option v-for="option in this.category_types_options" v-bind:value="option.name" v-bind:key="option.id">
@@ -16,19 +11,19 @@
                         </option>
                     </select>
                 </div>
-                <div style="float:left" class="mt-3">
-                    <span class="ml-3 go-btn go">
-                    <button v-on:click="saveImages()" type="button" class="btn btn-lg btn-outline-dark mr-5">Save</button>
-                    </span>
+            <div class="category-text">
+                <div>
+                    <h5>Enter a Wikimedia Commons categories</h5>
+                    <textarea class="" v-model="this.categories_input" type="text" placeholder="Ex: Category:African Foods, Category:Asian Clothes" />
                 </div>
             </div>
+            <button v-on:click="saveImages()" type="button" class="btn btn-lg btn-outline-dark mr-5 save">Save</button>
         </span>
     </div>
 
-    <div v-if="this.categories.length > 1" class="categories-table container text-center">
+    <div v-if="this.categories.length > 1" class="categories-table-display text-center">
         <h5 >Below are the categories which have been added</h5>
-        <br>
-        <table id="productsTable" class="table table-bordered mt-4">
+        <table id="productsTable" class="container text-center table table-bordered">
             <thead>
                 <tr>
                     <td>No</td>
@@ -62,7 +57,7 @@
         },
         data(){
             return{
-                category: null,
+                categories_input: null,
                 message: null,
                 selected_category_type: null,
                 category_types_options: [
@@ -80,7 +75,7 @@
         methods: {
             saveImages(){
                 axios.post(base_url + '/categories/add',{
-                    'category': this.category,
+                    'categories': this.categories_input,
                     'type': this.selected_category_type
                 })
                 .then(
@@ -121,21 +116,13 @@
 </script>
 
 <style>
-.category-text input{
-    width: 500px;
+.category-text textarea{
+    width: 40rem;
     line-height: 2rem;
-    float: left;
-    margin-top: 0.5rem;
 }
 
-.arrow{
-    font-size:  40px;
-    margin-top: 10px;
-}
-.next{
+.type-select{
     float: left;
-    font-size: 40px;
-    margin-top: 300px;
 }
 
 .go:hover{
@@ -161,26 +148,34 @@
     margin-top: 2.5rem;
 }
 
+.save{
+    float: left;
+    margin-left: 2rem;
+}
+
+.categories-table-display{
+    margin-top: 7rem;
+}
 @media only screen and (max-width: 600px) {
     h5{
         float: left;
         font-size: 1rem;
         margin-bottom: 1rem;
     }
-    .category-text input{
-        width: 22rem;
+    .category-text textarea{
+        width: 23rem;
+        float: left;
     }
     .cat-type{
-        width: 22rem;
+        width: 23rem;
         margin-left: 0px;
     }
-    .go-btn{
+    .save{
         margin-left: 0px;
-        margin-top: -20px;
-        margin-bottom: 1.5rem;
+        margin-top: 2rem;
     }
-    .categories-table{
-        margin-top: 10px !important;
+    .categories-table-display{
+    margin-top: 2rem;
     }
 }
 </style>
